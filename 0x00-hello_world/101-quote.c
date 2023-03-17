cbf4fbe88c3d3399c15a2953674eebf1ce5b8ae3
SammyYeboah <sammyyeboah001@gmail.com>
2023-03-17 10:06:52 -0700

    tired

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - entry point
  *
- * return: Always 0 (success)
+ * return: Always  (success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"stdout);
+	return (1);
 }
