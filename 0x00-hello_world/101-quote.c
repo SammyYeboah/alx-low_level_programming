5264c3f1064e79e99036de3d1f7dac626498fbd3
SammyYeboah <sammyyeboah001@gmail.com>
2023-03-17 09:55:57 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry
+ * main - entry point
  *
- * return: 0 (success)
+ * return: Always 0 (success)
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	return (0);
 }
