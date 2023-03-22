fc1a868cac453267bbb378f13643e3cd39064eaa
SammyYeboah <sammyyeboah001@gmail.com>
2023-03-22 08:45:12 -0700

    add my numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sums up two numbers
+ *
+ * @x: first parameter
+ * @y: second parameter
+ *
+ * Return: the sum
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
