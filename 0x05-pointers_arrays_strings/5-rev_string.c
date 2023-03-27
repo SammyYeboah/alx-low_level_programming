1721fdd9ec9b76dbcb23750efb1b8322165246db
SammyYeboah <sammyyeboah001@gmail.com>
2023-03-27 14:28:25 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *rev_string - reverse a string
+ *
+ *@s: parameter
+ *Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
