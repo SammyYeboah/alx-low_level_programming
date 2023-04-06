a0de9770c55ea668fa3e864a5a329ae9f8967590
SammyYeboah <sammyyeboah001@gmail.com>
2023-04-06 11:21:46 -0700

    exponential

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * _pow_recursion- function for exponential
+ * @x: first num
+ * @y: second num
+ * Return: as you can see in the code
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -8,5 +13,5 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 	else
-		return (x * _pow_recursion(x, y-1));
+		return (x * _pow_recursion(x, y - 1));
 }
