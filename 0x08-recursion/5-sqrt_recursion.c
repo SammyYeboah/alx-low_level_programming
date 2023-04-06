acfc5433b7bd8f44013dca76aaa24e1812a10806
SammyYeboah <sammyyeboah001@gmail.com>
2023-04-06 14:00:05 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,8 +8,11 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	int s;
+	if (s * s == n)
+		return (s);
+	else if (s * s > n)
 		return (-1);
 	else
-		return (_sqrt_recursion(n, 0));
+		return (_sqrt_recursion(n, s + 1));
 }
