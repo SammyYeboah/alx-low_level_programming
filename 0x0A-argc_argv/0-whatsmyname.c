75ef57c9711c5b2867d3f34b9630bd8f1c713e2c
SammyYeboah <sammyyeboah001@gmail.com>
2023-04-08 13:38:35 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- function to print code's name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
