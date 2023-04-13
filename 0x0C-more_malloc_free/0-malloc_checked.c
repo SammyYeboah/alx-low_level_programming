abe1349f3d613768d11d832b81b6f4248237e926
SammyYeboah <sammyyeboah001@gmail.com>
2023-04-13 06:50:49 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked- allocate memory
+ * @b: memory space
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b)
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
